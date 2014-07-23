7a44d12e102ba580f1a43c9f655da2753722b132
Mathias-Larsen <Mathias.e.larsen@gmail.com>
2014-07-23 10:36:50 +0200

    Fun stuff on the way

--- scripts/awesomeMain.js
+++ scripts/awesomeMain.js
@@ -35,13 +35,31 @@ $(function () {
     SyntaxHighlighter.all();
 });
 
+var someList = [
+    'Mathias', 'Is', 'Super', 'Duper', 'Handsome'
+];
+
+var injuryObjects = [
+    {
+        code: 'A.1.1.1.2',
+        injury: 'Han har det ikke så godt'
+    },
+    {
+        code: 'A.1.1.1.3',
+        injurty: 'Han har det fint'
+    }
+];
+
+var words = ['ASH - Årsagssammenhæng','nvm - nevermind','lol - laughing out loud','brb - be right back','rofl - rolling on the floor laughing'];
+
 var injuryCodes = ['D.1.1.1.4 - Tab af tommelfingers yderstyrke - 12%', 'D.1.1.1.5 - Tab af tommelfingers 1/2 yderstykre - 8%',
-    'D.1.1.1.6 - Tommfinger med stift yderled i god stilling - 5%', 'D.1.2.1.7 - Tommelfinger med stift grundled i god stilling - <5%'];
-var techCompanies = ['Apple', 'Microsoft'];
+    'D.1.1.1.6 - Tommfinger med stift yderled i god stilling - 5%', 'D.1.2.1.7 - Tommelfinger med stift grundled i god stilling - <5%',
+    'ASH - Årsagssammenhæng'];
 
 $('#textarea4').textcomplete([
     { // injury code strategy
-        match: /\B:([\-+.\w]*)$/,
+        match: /\B:\b([\-+.\w{2,}]*)$/,
+
         search: function (term, callback) {
             callback($.map(injuryCodes, function(injuryCode) {
                 return injuryCode.indexOf(term) === 0 ? injuryCode : null;
@@ -51,10 +69,55 @@ $('#textarea4').textcomplete([
             return injuryCode + ' ';
         },
         index: 1,
+        maxCount: 5,
         placement: top
-
     }
 ]);
+
+
+$('#textarea4').textcomplete([
+    { // word shortcut
+        match: /\b(\w{2,})$/,
+        search: function (term, callback) {
+            callback($.map(words, function (word) {
+                return word.indexOf(term) === 0 ? word : null;
+            }));
+        },
+        replace: function(word) {
+
+            var splitList = word.split('-');
+
+            return splitList[1].replace(/\s?/, '') + ' ';
+            //console.log(splitList);
+            //return splitList[1].splitText(1);
+
+            //return splitList[splitList.length-1] + ' ';
+        },
+        index: 1,
+        maxCount: 5,
+        placement: top
+    }
+]);
+
+$('#textarea2').textcomplete([
+    { // emoji Strategy
+        match: /\B:([\-+\w]*)$/,
+        search: function(term, callback) {
+            callback($.map(emojies, function(emoji) {
+                return emoji.indexOf(term) === 0 ? emoji : nullM
+            }));
+
+        },
+        template: function(value) {
+            return '<img src="styles/images/emoji/' + value + '.png"></img>' + value;
+        },
+        replace: function (value) {
+            return someList[emojies.indexOf(value)] + ' ';
+        },
+        index: 1,
+        maxCount: 7
+    }
+])
 
 
 
